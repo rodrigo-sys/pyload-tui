3e207f8aeaf8f42291b6a4aac222a08a0c3c0546
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-05-06 16:21:40 -0300

    feat(ui): render packages table
    - Add crossterm for keyboard event handling
    - Add tokio to allow main async
    - Implement keyboard binds 'j/k' for navigation 'q' for exit
    - Render table with packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{self, Event, KeyCode};
+use pyload_tui::table::PackagesTable;
+use pyload_tui::utils::fetch_packages;
+use ratatui::widgets::{Table, TableState};
+use tokio::main;
+
+#[main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let packages = fetch_packages().await?;
+    let table: Table = PackagesTable::from(packages).0;
+
+    let mut table_state = TableState::new();
+    table_state.select_first();
+
+    ratatui::run(|terminal| {
+        loop {
+            terminal.draw(|frame| {
+                frame.render_stateful_widget(&table, frame.area(), &mut table_state)
+            })?;
+
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Char('j') => table_state.select_next(),
+                    KeyCode::Char('k') => table_state.select_previous(),
+                    _ => (),
+                }
+            }
+        }
+        Ok(())
+    })
 }
