8eef9020b1fd74d99bc225733b62bffc3ea49832
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-05-06 19:50:20 -0300

    refactor(main): make use of app struct
    - Implement and use app struct instead of old approach

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
-use crossterm::event::{self, Event, KeyCode};
+use crossterm::event::{self, Event};
+use pyload_tui::app::App;
+use pyload_tui::current_screen::CurrentScreen;
 use pyload_tui::table::PackagesTable;
 use pyload_tui::utils::fetch_packages;
-use ratatui::widgets::{Table, TableState};
 use tokio::main;
 
 #[main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let packages = fetch_packages().await?;
-    let table: Table = PackagesTable::from(packages).0;
+    let packages_table = PackagesTable::from(packages.clone()).0;
 
-    let mut table_state = TableState::new();
-    table_state.select_first();
+    let mut app = App::new();
+    app.packages = packages;
+    app.packages_table_state.select_first();
 
     ratatui::run(|terminal| {
         loop {
             terminal.draw(|frame| {
-                frame.render_stateful_widget(&table, frame.area(), &mut table_state)
+                match app.current_screen {
+                    CurrentScreen::Packages => {
+                        frame.render_stateful_widget(
+                            &packages_table,
+                            frame.area(),
+                            &mut app.packages_table_state,
+                        )
+                    }
+                    CurrentScreen::Files => {},
+                    _ => {}
+                }
             })?;
 
             if let Event::Key(key) = event::read()? {
                 match key.code {
-                    KeyCode::Char('q') => break,
-                    KeyCode::Char('j') => table_state.select_next(),
-                    KeyCode::Char('k') => table_state.select_previous(),
-                    _ => (),
+                    crossterm::event::KeyCode::Char('q') => break,
+                    _ => app.handle_key(key.code),
                 }
             }
         }
