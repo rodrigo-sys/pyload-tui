dc5ee441b3921d4b70a80d8ceddf94d755bc33db
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-05-17 09:58:36 -0300

    refactor: rename get_config to get_pyload_config

--- src/utils.rs
+++ src/utils.rs
@@ -10,7 +10,7 @@ use openapi::models::{
     ApiAddFilesPostRequest, ApiAddPackagePostRequest, ApiSetPackageDataPostRequest, Destination,
 };
 
-fn get_config() -> Configuration {
+fn get_pyload_config() -> Configuration {
     let api_url = std::env::var("API_URL").unwrap_or_else(|_| "http://localhost:8000".to_string());
     let api_key = std::env::var("API_KEY").expect("API_KEY must be set");
 
@@ -24,11 +24,11 @@ fn get_config() -> Configuration {
 }
 
 pub async fn fetch_packages() -> Result<Vec<openapi::models::PackageData>, String> {
-    let mut queue = py_load_rest_api::api_get_queue_get(&get_config())
+    let mut queue = py_load_rest_api::api_get_queue_get(&get_pyload_config())
         .await
         .map_err(|e| e.to_string())?;
 
-    let collector = py_load_rest_api::api_get_collector_get(&get_config())
+    let collector = py_load_rest_api::api_get_collector_get(&get_pyload_config())
         .await
         .map_err(|e| e.to_string())?;
 
@@ -38,7 +38,7 @@ pub async fn fetch_packages() -> Result<Vec<openapi::models::PackageData>, Strin
 }
 
 pub async fn fetch_files(package_id: i32) -> Result<Vec<openapi::models::FileData>, String> {
-    let pkg = py_load_rest_api::api_get_package_data_get(&get_config(), Some(package_id))
+    let pkg = py_load_rest_api::api_get_package_data_get(&get_pyload_config(), Some(package_id))
         .await
         .map_err(|e| e.to_string())?;
 
@@ -55,12 +55,12 @@ pub async fn add_package(
 ) -> Result<i32, Error<ApiAddPackagePostError>> {
     let mut pkg = ApiAddPackagePostRequest::new(name, links);
     pkg.dest = Some(dest);
-    let pid = api_add_package_post(&get_config(), Some(pkg)).await?;
+    let pid = api_add_package_post(&get_pyload_config(), Some(pkg)).await?;
 
     if let Some(pw) = password && !pw.is_empty() {
         let data = HashMap::from([("password".to_string(), serde_json::Value::from(pw))]);
         let req = ApiSetPackageDataPostRequest::new(pid, data);
-        let _ = api_set_package_data_post(&get_config(), Some(req)).await;
+        let _ = api_set_package_data_post(&get_pyload_config(), Some(req)).await;
     }
 
     Ok(pid)
@@ -71,5 +71,5 @@ pub async fn add_links_to_package(
     links: Vec<String>,
 ) -> Result<(), Error<ApiAddFilesPostError>> {
     let req = ApiAddFilesPostRequest::new(package_id, links);
-    api_add_files_post(&get_config(), Some(req)).await
+    api_add_files_post(&get_pyload_config(), Some(req)).await
 }
