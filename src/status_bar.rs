5226eeaf214e422b5218e7db020d72491ae94ab7
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-07-11 17:25:38 -0300

    feat: create status bar widget skeleton

---
+++ src/status_bar.rs
@@ -0,0 +1,13 @@
+use ratatui::{
+    prelude::{Buffer, Rect},
+    widgets::Widget,
+};
+
+struct StatusBar {
+}
+
+impl Widget for StatusBar {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        todo!()
+    }
+}
