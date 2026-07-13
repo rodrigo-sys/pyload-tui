828eb9e09ac6c5eabf6e6c0c1d71b55b4a660a84
Rodrigo Chavez <chavezrodrigo001@gmail.com>
2026-07-12 23:02:54 -0300

    feat(status-bar): assign real values from server status

--- src/status_bar.rs
+++ src/status_bar.rs
@@ -1,27 +1,22 @@
+use openapi::models::ServerStatus;
 use ratatui::{
-    layout::Constraint,
-    prelude::{Buffer, Rect},
-    style::{Color, Style},
-    widgets::{Block, Borders, Row, Table, Widget},
+    layout::Constraint, prelude::{Buffer, Rect}, style::{Color, Style},     widgets::{Block, Borders, Row, Table, Widget},
 };
 
 #[derive(Clone)]
 pub struct StatusBar {
-    table: Table<'static>,
+    server_status: Option<ServerStatus>,
 }
 
 impl StatusBar {
     pub fn new() -> Self {
-        let rows = vec![
-            Row::new(vec!["QUEUE:", "PAUSED"]).style(Style::new().fg(Color::Yellow).bold()),
-            Row::new(vec!["ACTIVE:", "0"]).style(Style::new().fg(Color::Magenta).bold()),
-            Row::new(vec!["SPEED:", "-"]).style(Style::new().fg(Color::Blue).bold()),
-        ];
+        Self {
+            server_status: None,
+        }
+    }
 
-        let table = Table::new(rows, vec![Constraint::Length(10), Constraint::Length(10)])
-            .block(Block::default().borders(Borders::ALL));
-
-        Self { table }
+    pub fn refresh(&mut self, server_status: ServerStatus) {
+        self.server_status = Some(server_status);
     }
 }
 
@@ -33,6 +28,38 @@ impl Default for StatusBar {
 
 impl Widget for StatusBar {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        self.table.render(area, buf);
+        if let Some(status) = self.server_status {
+            StatusTable::from(status).0.render(area, buf);
+        }
+    }
+}
+
+pub struct StatusTable(pub Table<'static>);
+
+impl From<ServerStatus> for StatusTable {
+    fn from(server_status: ServerStatus) -> Self {
+        let rows = vec![
+            Row::new(vec![
+                "QUEUE:",
+                if server_status.pause {
+                    "PAUSED"
+                } else {
+                    "UNPAUSED"
+                },
+            ])
+            .style(Style::new().fg(Color::Yellow).bold()),
+            Row::new(vec![
+                "ACTIVE:".to_string(),
+                server_status.active.to_string(),
+            ])
+            .style(Style::new().fg(Color::Magenta).bold()),
+            Row::new(vec!["SPEED:".to_string(), server_status.speed.to_string()])
+                .style(Style::new().fg(Color::Blue).bold()),
+        ];
+
+        let table = Table::new(rows, vec![Constraint::Length(10), Constraint::Length(10)])
+            .block(Block::default().borders(Borders::ALL));
+
+        StatusTable(table)
     }
 }
